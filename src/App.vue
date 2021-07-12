fc66d08dd4ba3714e88203c034c9538c8d888490
Aakash Thapa <aakashmanm3@gmail.com>
2021-07-12 05:47:06 +0545

    Project Init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Nigga Reaction Timer </h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Result :showResults="showResults" :score="score"></Result>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from './components/Block.vue'
+import Result from './components/Results.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    Result
+  },
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -23,4 +46,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+button{
+  padding: 10px 24px;
+  border: none;
+  outline: none;
+  color:#fff;
+  background: rgb(107, 107, 240);
+  border-radius: 5px;
+}
+
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+    <div v-if="showResults">
+        <p >Reaction Time:{{score}} ms</p>
+        <h2>{{result}}</h2>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Result',
+    props: ["score","showResults"],
+    computed: {
+        result(){
+
+            if(this.score <= 250){
+                return "Your Got Talent Kiddo..."
+            }
+            else if(this.score <= 450){
+                return "Wow Nigga!! That's Good..."
+            }
+            else{
+                return "Snail Pace Nigga...."
+            }
+        }
+    }
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
